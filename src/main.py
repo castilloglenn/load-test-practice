94d6473583289b3ada87cd3dd3aa01744d3123dd
dev-glenn-lr <dev_glenn@lrtechs.co.jp>
2025-05-19 11:38:28 +0800

    build: reorganize dependencies in pyproject.toml and remove uvicorn run from main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-import uvicorn
 
 app = FastAPI()
 
@@ -8,7 +7,3 @@ app = FastAPI()
 @app.get("/", response_class=PlainTextResponse)
 def hello():
     return "Hello, World!"
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
